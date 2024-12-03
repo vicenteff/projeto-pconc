1532215244c878b07497dcb9197d0cbdd6ec71e6
Vicente Ferreira <vicente.frazao.ferreira@tecnico.ulisboa.pt>
2024-12-03 13:24:45 +0000

    (3/12/24) Implemented ordering of files

--- 2024-to_students/old-photo-serial/old-photo-parallel-A.c
+++ 2024-to_students/old-photo-serial/old-photo-parallel-A.c
@@ -4,23 +4,67 @@
 #include <dirent.h> 
 #include <sys/stat.h>
 
+#define DIRECTORY argv[1]
+#define THREADS argv[2]
+#define ORDER argv[3]
+
+typedef struct
+{
+    char *filename;
+    int size;
+} Fileinfo;
+
+int compare_by_name(const void *a, const void *b) {
+    Fileinfo *file1 = (Fileinfo *)a;
+    Fileinfo *file2 = (Fileinfo *)b;
+    return strcmp(file1->filename, file2->filename);
+}
+
+int compare_by_size(const void *a, const void *b) {
+    Fileinfo *file1 = (Fileinfo *)a;
+    Fileinfo *file2 = (Fileinfo *)b;
+    return file1->size - file2->size;
+}
+
 char *get_extension(const char *filename) {
     char *dot = strrchr(filename, '.');
     if(!dot || dot == filename) return "";
     return dot;
 }
 
-int listdir(char *directory, char *extension) {
+int filecount(char *directory, char *extension) {
   DIR *d;
+  int counter = 0;
   struct dirent *f;
   d = opendir(directory);
   if (d) {
-    while (((f = readdir(d)) != NULL) /*&& !strcmp(get_extension(f->d_name), extension)*/) {
+    while ((f = readdir(d)) != NULL) {
+        if(!strcmp(get_extension(f->d_name), extension)) {
+            counter++;
+        }
+    }
+    closedir(d);
+  }
+  return counter;
+}
+
+int listdir(char *directory, char *extension, Fileinfo *fileinfo) {
+  DIR *d;
+  int i = 0;
+  struct dirent *f;
+  d = opendir(directory);
+  if (d) {
+    while (((f = readdir(d)) != NULL)) {
         if(!strcmp(get_extension(f->d_name), extension)) {
             printf("%s ", f->d_name);
+            strcpy(fileinfo[i].filename, f->d_name);
+
             struct stat st;
             stat(f->d_name, &st);
             printf("%ld\n", st.st_size);
+            fileinfo[i].size = st.st_size;
+
+            i++;
         }
     }
     closedir(d);
@@ -28,15 +72,34 @@ int listdir(char *directory, char *extension) {
   return(0);
 }
 
+int sort_fileinfo(Fileinfo *fileinfo, char *order, int n_files) {
+
+    if(!strcmp(order, "-name")) {
+        qsort(fileinfo, n_files, sizeof(Fileinfo), compare_by_name);
+    }
+    else if(!strcmp(order, "-size")) {
+        qsort(fileinfo, n_files, sizeof(Fileinfo), compare_by_size);
+    }
+    else    
+        return -1;
+
+    return 0;
+
+}
+
 int main(int argc, char *argv[]) {
+
+/*********************************************************************************/
+/*PROGRAM ARGUMENTS                                                              */
+/*********************************************************************************/
 
     char *dir = malloc(100 * sizeof(char));
     char *order = malloc(100 * sizeof(char));
-    int threads;
+    int threads, n_files;
 
-    strcpy(dir, argv[1]);
-    threads = atoi(argv[2]);
-    strcpy(order, argv[3]);
+    strcpy(dir, DIRECTORY);
+    threads = atoi(THREADS);
+    strcpy(order, ORDER);
 
     if(threads < 1) {
         perror("INVALID NUMBER OF THREADS\n");
@@ -48,12 +111,23 @@ int main(int argc, char *argv[]) {
         exit(2);
     }
 
-    listdir(argv[1], ".jpeg");
+/*********************************************************************************/
+
+    n_files = filecount(DIRECTORY, ".jpeg");
+
+    Fileinfo *fileinfo = malloc(n_files * sizeof(Fileinfo));
+    for (int i = 0; i<n_files; i++)
+        fileinfo[i].filename = malloc(100 * sizeof(char));
+
+    listdir(DIRECTORY, ".jpeg", fileinfo);
+
+    sort_fileinfo(fileinfo, ORDER, n_files);
+
+    /*for(int i = 0; i<n_files; i++) {
+        printf("name: %s size: %i \n", fileinfo[i].filename, fileinfo[i].size);
+    }*/
 
 
-
-
-
-    printf("IN DIRECTORY: %s\nNUMBER OF THREADS: %d\nORDER BY: %s\n", dir, threads, order);
+    //sprintf("IN DIRECTORY: %s\nNUMBER OF THREADS: %d\nORDER BY: %s\n", dir, threads, order);
 
 }
